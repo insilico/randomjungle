26de6e56597a1e738d0e6ca9191f0c115dd9c804
Bill White <bill.c.white@gmail.com>
2012-07-01 23:28:17 -0500

    Using AC_CHECKLIB versus AC_OPENMP.

--- config.h
+++ config.h
@@ -16,6 +16,9 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the `gomp' library (-lgomp). */
+#define HAVE_LIBGOMP 1
+
 /* Define to 1 if you have the `pthread' library (-lpthread). */
 #define HAVE_LIBPTHREAD 1
 
